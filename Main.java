956d846992c17761cd79ec228024a3d601f042da
Ilya <127449810+sumoomus@users.noreply.github.com>
2023-03-16 17:07:22 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        int a=1,b=1,c=0;
+        System.out.print(a+" "+b+" ");
+        for(int i=3;i<=11;i++){
+            c=a+b;
+            a=b;
+            b=c;
+            System.out.print(c+" ");
+                }
+            }
+        }
